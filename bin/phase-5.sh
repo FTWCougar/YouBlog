22233e91bf5f627b29fc576c89df8bf603157e21
Cameron <Cameron@dansclan.com>
2023-01-12 15:54:54 -0800

    Blog Cards and Rerouting working

--- bin/phase-5.sh
+++ bin/phase-5.sh
@@ -5,7 +5,5 @@ npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rake db:drop
-bundle exec rake db:create
 bundle exec rake db:migrate
 bundle exec rake db:seed
